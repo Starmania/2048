8e7176b37cf494cc270114f9a3841b2c89c7bb5a
justin01510 <justin.plagnard@gmail.com>
2025-01-10 16:04:10 +0100

    Update afficherscore.c

--- afficher_les_scores/afficherscore.c
+++ afficher_les_scores/afficherscore.c
@@ -1,26 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-    int *tabscore;
-    int nbjoueur;
+int affichagetab() {
     int i;
-    printf("Combien de joueurs êtes-vous : ");
-    scanf("%d", &nbjoueur);
-
-    tabscore = (int *)calloc(nbjoueur, sizeof(int));
-    if (tabscore == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
-        return 1;
-    }        
     printf("| ");
-    for(int i = 0; i < nbjoueur; i++){
-        printf("   joueur %d   | ", i+1);}
+    for (i = 0; i < nbjoueur; i++) {
+        printf("   joueur %d   | ", i + 1);
+    }
     printf("\n| ");
-    
-    for(int i = 0; i < nbjoueur; i++)
+
+    for (i = 0; i < nbjoueur; i++) {
         printf("%10d    | ", tabscore[i]);
-    
-    free(tabscore);
+    }
+    printf("\n");
 
     return 0;
 }
