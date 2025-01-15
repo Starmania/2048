5bb7eaa211b700e923f17408108053b3af94a9be
justin01510 <justin.plagnard@gmail.com>
2025-01-15 10:16:43 +0100

    Create meilleur_score.c

---
+++ afficher_les_scores/meilleur_score.c
@@ -0,0 +1,8 @@
+void classement(int *tabscore, int nbjoueur);
+void classement(int *tabscore,int nbjoueur){
+    for(int i=0;i<nbjoueur;i++){
+        if (tabscore[i]>scoremax){
+            scoremax=tabscore[i];
+        }
+    }
+}
