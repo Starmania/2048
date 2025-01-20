129d2e32fb06d3dcbe006038e2a7847f07ed0c62
Starmania <wycvhrt6vzscfpedxr@gmail.com>
2025-01-20 16:16:47 +0100

    Ajout de fonctions et test pour gérer dynamiquement une liste de noms

--- coeur/pasouf.c
+++ coeur/pasouf.c
@@ -1,32 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int ajouterNom(char ***noms, int *taille, char *nom)
 {
-    // Allocate memory for 4 pointers to char
-    char **noms = malloc(4 * sizeof(char *));
-    if (!noms)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        return 1;
-    }
-
-    noms[0] = "Jean";
-    noms[1] = "Paul";
-    noms[2] = "Jacques";
-    noms[3] = "Marie";
-
-    char **noms_nouveau = realloc(noms, 5 * sizeof(char *));
+    char **noms_nouveau = realloc(*noms, (*taille + 1) * sizeof(char *));
     if (!noms_nouveau)
     {
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
-    noms = noms_nouveau;
-    noms[4] = "Pierre";
+    *noms = noms_nouveau;
+    (*noms)[*taille] = nom;
+    (*taille)++;
+    return 0;
+}
 
-    // Free the allocated memory
-    free(noms);
+int enleverTousLesNoms(char ***noms, int *taille)
+{
+    for (int i = 0; i < *taille; i++)
+    {
+        free((*noms)[i]);
+    }
+    free(*noms);
+    *noms = NULL;
+    *taille = 0;
+    return 0;
+}
+
+// Testé avec gcc -O0 + valgrind.
+// Pas de fuite de mémoire :)
+int tester(char ***noms, int *taille)
+{
+    for (size_t i = 0; i < 8; i++)
+    {
+        ajouterNom(noms, taille, strdup("John Doe"));
+        ajouterNom(noms, taille, strdup("Doe John"));
+    }
+
+    printf("Taille: %d\n", *taille);
+
+    // Enlève tous les noms
+    enleverTousLesNoms(noms, taille);
+
+    printf("Taille: %d\n", *taille);
+}
+
+int main()
+{
+    // Ajoute des noms à la liste
+    char **noms = NULL;
+    int taille = 0;
+    tester(&noms, &taille);
+    tester(&noms, &taille);
+    printf("Taille en dehors 1: %d\n", taille);
+    tester(&noms, &taille);
+    printf("Taille en dehors 1: %d\n", taille);
 
     return 0;
 }
